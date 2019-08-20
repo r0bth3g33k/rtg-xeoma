78b3d6a1b2557b4ae054e049891b6a5184d1b9b2
r0bth3g33k <54051357+r0bth3g33k@users.noreply.github.com>
2019-08-20 16:30:06 -0600

    Update xeoma.sh

--- xeoma.sh
+++ xeoma.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 
-if [ ! -f "/usr/local/Xeoma/.firstrun" ]
-then
-	ls -al /usr/local/Xeoma/.firstrun
-	/root/xeoma.app -setpassword 'ChangeMe!'
+	/root/xeoma.app -setpassword ChangeMe
 	touch /usr/local/Xeoma/.firstrun
-fi
 
 /root/bin/Xeoma/xeoma -showpassword
 /root/bin/Xeoma/xeoma -- -service -log -startdelay 5
